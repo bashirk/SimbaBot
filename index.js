e6f3a5fe80ceb306dfa2c07dd3b42ab534b984fc
Korede Bashir <bashirkorede@gmail.com>
2020-06-15 13:16:41 +0100

    make it better

--- index.js
+++ index.js
@@ -43,18 +43,28 @@ const bot_config = {
 
 const bot = new Bot(bot_config);
 
+const handlePostback = (_sender_info, received_postback) => {
+    let response;
+ 
+    // Get the payload for the postback
+    let payload = received_postback.payload;
+ 
+    if(payload === 'GET_STARTED'){
+        response = askTemplate('Are you interested in learning more information about your experience with us?');
+        callSendAPI(sender_psid, response);
+    }
+}
 
-
-webhook.on("messages", (event_type, sender_info, webhook_event, sender_psid, received_postback) => {
+webhook.on("messages", (event_type, sender_info, webhook_event, received_postback) => {
     logEvent(event_type, sender_info, webhook_event);
     bot.handleText(event_type, sender_info, webhook_event);
-    bot.handlePostback(sender_psid, received_postback);
+    handlePostback(sender_info, received_postback);
 });
 
 webhook.on("messaging_postbacks", (event_type, sender_info, webhook_event, sender_psid, received_postback) => {
     logEvent(event_type, sender_info, webhook_event);
     bot.handleText(event_type, sender_info, webhook_event);
-    bot.handlePostback(sender_psid, received_postback);
+    handlePostback(sender_info, received_postback);
 });
 
 webhook.on("messaging_handovers", (event_type, sender_info, webhook_event) => {
