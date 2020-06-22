1810011767d6dd6cda6cf4f4435a92c458eccb42
Korede Bashir <bashirkorede@gmail.com>
2020-06-22 02:05:12 +0100

    modified Get Started payload event

--- app/bot.js
+++ app/bot.js
@@ -101,22 +101,14 @@ class Bot {
         else if ((webhook_event.postback &&
             webhook_event.postback.payload === PAYLOADS.COMMAND_PAYLOADS.GET_STARTED)
         ) {
-            const quick_replies = PAYLOADS.CONFIRM_HANDOVER_QUICK_REPLIES.map(qr => {
-                return {
-                    content_type: "text",
-                    title: qr.title,
-                    payload: qr.payload,
-                }
-            });
-
-            try {
-                this.messenger_client.sendQuickReplies(
-                    recipient,
-                    quick_replies,
-                    "Are you interested in talking to a real human support?");
-            } catch(e) {
-                console.error(e);
-            }
+            await this.messenger_client.sendText(
+                recipient,
+                "Hi there! I'm Simba Bot 🦁, personal assistant for DeliveryNow NG, and I'm here to make your life better. I do not know much, but I definitely learn from conversations to get better, I also have smart human friends who can help if I cannot answer your question. 🤓",
+            );
+            await this.messenger_client.sendText(
+                recipient,
+                "So. How can I help you today? 🙂",
+            );
         }
 
         // Direct request for agent through persistent menu or text entry
