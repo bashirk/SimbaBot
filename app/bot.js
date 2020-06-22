019c94872ed03dbe9e2d94d2f73a7e105fc1e88c
Korede Bashir <bashirkorede@gmail.com>
2020-06-22 02:37:04 +0100

    fixed persistent menu

--- app/bot.js
+++ app/bot.js
@@ -294,27 +294,6 @@ class Bot {
                 );
             }
 
-            // listen for webhook message event
-            else if ((webhook_event.message.text.includes("talk") && webhook_event.message.text.includes("agent") || webhook_event.message.text.includes("support"))
-            ) {
-                const quick_replies = PAYLOADS.CONFIRM_HANDOVER_QUICK_REPLIES.map(qr => {
-                    return {
-                        content_type: "text",
-                        title: qr.title,
-                        payload: qr.payload,
-                    }
-                });
-
-                try {
-                    this.messenger_client.sendQuickReplies(
-                        recipient,
-                        quick_replies,
-                        "Are you interested in talking to a real human support?");
-                } catch(e) {
-                    console.error(e);
-                }
-            }
-
             else if (webhook_event.message.text.includes("About") && webhook_event.message.text.includes("Company")) {
                 let generic_template = {
                     template_type: "generic",
@@ -342,6 +321,27 @@ class Bot {
                 try {
                     this.messenger_client.sendTemplate(recipient, generic_template)
                 } catch (e) {
+                    console.error(e);
+                }
+            }
+
+            // listen for webhook message event
+            else if ((webhook_event.message.text.includes("talk") && webhook_event.message.text.includes("agent") || webhook_event.message.text.includes("support"))
+            ) {
+                const quick_replies = PAYLOADS.CONFIRM_HANDOVER_QUICK_REPLIES.map(qr => {
+                    return {
+                        content_type: "text",
+                        title: qr.title,
+                        payload: qr.payload,
+                    }
+                });
+
+                try {
+                    this.messenger_client.sendQuickReplies(
+                        recipient,
+                        quick_replies,
+                        "Are you interested in talking to a real human support?");
+                } catch(e) {
                     console.error(e);
                 }
             }
